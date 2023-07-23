67e9b14832affb6c3caab36e039bd671952e6b2f
shahadat10033 <shahadat10033@gmail.com>
2023-07-24 05:36:38 +0600

    creating get api for collegedbCollection

--- index.js
+++ index.js
@@ -28,6 +28,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const collegesDbCollection = client.db("collegeDB").collection("colleges");
+
+
+app.get("/colleges", async(req,res)=>{
+
+    const result = await collegesDbCollection.find().toArray();
+    res.send(result);
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
