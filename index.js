7d10b056d1894177242c06dbfba856a595ba07a3
shahadat10033 <shahadat10033@gmail.com>
2023-07-24 03:27:10 +0600

    installing dot env and cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const cors = require("cors");
+require("dotenv").config();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
